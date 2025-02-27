2a5a2f5fb4a5a8ed8d44e75aa8d6d84ff57b6e36
Ari Alamsyah <arialamsyah@MacBook-Air-Ari.local>
2025-02-27 09:34:54 +0700

    Set Up SchadCN UI and Light & Dark Theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+
+  
+
 const Home = () => (
 <>
 <h1 className="text-3xl text-primary-200 h1-bold font-inter">Welcome to Next.js</h1>
-<h1 className="text-3xl text-primary-200 h1-bold font-space-grotesk ">Welcome to Next.js</h1>
+
+
 </>
 )
 
