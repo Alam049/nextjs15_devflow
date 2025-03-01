1cd000b0b09783e131edd9e0eec798d9fedc558f
Ari Alamsyah <arialamsyah@MacBook-Air-Ari.local>
2025-03-01 16:26:38 +0700

    New Branch starter-dev

--- components/navigation/LeftSidebar.tsx
+++ components/navigation/LeftSidebar.tsx
@@ -6,6 +6,7 @@ import ROUTES from '@/constants/routes'
 import Image from 'next/image'
 
 const LeftSidebar = () => {
+    // 
   return (
    <section className='custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 h-screen flex flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]'>
     <div className='flex flex-1 flex-col gap-6'>
